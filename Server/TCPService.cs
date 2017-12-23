3601fcbe1643b74678141e6a699708c0291c0b96
GoodGoodMan <gjwnsan9420@gmail.com>
2017-12-23 11:16:02 +0900

    Init repository.

---
+++ Server/TCPService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GGM.Application.Service;
+
+namespace Server
+{
+    public class TCPService : IService
+    {
+        public Guid ID { get; set; }
+
+        public void Boot(string[] arguments)
+        {
+            //TODO: Start Server
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GGM.Application;
+using Server;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GGMApplication.Run(typeof(Program), args, typeof(TCPService));
+        }
+    }
+}
